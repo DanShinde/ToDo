c75b849d4b1fdf37eda8721350fd5f5351924e91
DanShinde <pravin113347@gmail.com>
2023-04-05 23:46:44 +0530

    Updated wsgi

--- ToDo/ToDo/wsgi.py
+++ ToDo/ToDo/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
 
+import os
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDo.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
